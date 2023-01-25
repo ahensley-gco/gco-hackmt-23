c0f667dc378f70c8dcfdb4e1cdf3b76ad3f4f42a
Hensley <ahensley@genesco.com>
2023-01-25 17:43:06 -0600

    added image storage

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,11 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolId": "string",
       "UserPoolName": "string"
     }
+  },
+  "storage": {
+    "speedata": {
+      "BucketName": "string",
+      "Region": "string"
+    }
   }
 }
