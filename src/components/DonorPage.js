d1c218a0efd3aae5c284bdc44a9acb56418936dc
Kwabena Fosuhene <kof2c@mtmail.mtsu.edu>
2023-01-29 07:19:21 -0600

    progress

--- src/components/DonorPage.js
+++ src/components/DonorPage.js
@@ -1,8 +1,10 @@
-import { Flex, Link, Loader } from "@aws-amplify/ui-react";
-import { useCallback, useEffect, useState } from "react"
+import { Flex, Link, Loader, Text } from "@aws-amplify/ui-react";
+import { useCallback, useEffect, useState, React } from "react"
 import { listNotes, listSpees } from "../graphql/queries";
 import { API } from "aws-amplify";
 import { useNavigate } from "react-router-dom";
+import { getDonor, getMedia } from "./fakedata";
+import MediaPost from "./MediaPost";
 
 
 
@@ -11,6 +13,7 @@ const DonorPage = () => {
 
     const [donorName, setDonorName] = useState('');
     const [sponsorees, setSponsorees] = useState([]);
+    const [media, setMedia] = useState([]);
 
     const fetchSponsorees = useCallback(async () => {
         const apiData = await API.graphql({ query: listSpees });
@@ -21,20 +24,30 @@ const DonorPage = () => {
 
     useEffect(() => {
         fetchSponsorees().then((data) =>  {setSponsorees(data)});
-        // console.log(list);
-        // setSponsorees(list);
+        getMedia().then((data) => setMedia(data));
+        getDonor().then(({name}) => setDonorName(name));
     }, [setSponsorees, fetchSponsorees])
 
     return (
-        (sponsorees) ?
+        (sponsorees && media) ?
         <>
-        <div>Hi, {'donorName'}</div>
-        <div>Wanna know how your sponsorees are doing? Here they are; click on their name to learn more.</div>
+        <Flex direction="column" padding="relative.large" paddingBottom="xs">
+        <h1>Hi, {donorName}</h1>
+        <h3>Wanna know how your sponsorees are doing? Here they are!</h3>
         <ul>
-        {sponsorees.map(({name}) => <li><Link href="sponsoree" onClick={navigate}>{name}</Link></li>)}
+        {sponsorees.map(({name}) => <li key={name}><Text>{name}</Text></li>)}
         </ul>
+        </Flex> 
+        <Flex direction="column" alignItems="center" justifyContent="center" padding="large" paddingTop="none">
+        <h2>Media Page</h2>
         
-        </> 
+        {media.map((data, index) => 
+        <div key={data.description}>
+        <MediaPost  props={{...data, index}} />
+        <br/>
+        </div>)}
+        </Flex>
+        </>        
         : <Flex width='100vw' height="100vh" alignItems="center" justifyContent="center">
             <Loader width="100px" size="large" variation="linear"/>
         </Flex>
---
+++ src/components/MediaPost.js
@@ -0,0 +1,66 @@
+import {   Card,
+   Image,
+   View,
+   Heading,
+   Flex,
+   Badge,
+   Text,
+   Button,
+   useTheme, } from "@aws-amplify/ui-react";
+
+
+const MediaPost = ({props: {name, image, description, index, timestamp, title}}) => {
+
+
+    return (
+   //      <Card>
+   //       <Image alt={props.description} src={props.image} width="30vw" height="30vh"></Image>
+   //   {/* <Flex width='10vw' height="10vh" alignItems="center" justifyContent="center"> */}
+   //   asdfsdafsadfa
+   //      <div>{props.name}</div>
+   //      <div>{props.timestamp}</div>
+   //      <img src={props.image} alt={props.name} width="30vw" height="30vh"/>
+   //      <div>{props.description}</div>
+   //   {/* </Flex> */}
+   //      </Card>)
+   <View
+      backgroundColor={'pink'}
+      padding={'30px'}
+    >
+      <Card>
+        <Flex direction="row" alignItems="flex-start">
+          <Image
+            alt={description}
+            src={require(`../images/${index}.jpeg`)}
+            minWidth="30vw"
+            width="200px"
+            height="200px"
+          />
+          <Flex
+            direction="column"
+            alignItems="flex-start"
+            gap={'10px'}
+          >
+            <Flex>
+              <Badge size="small" variation="info">
+                {timestamp}
+              </Badge>
+              <Badge size="small" variation="success">
+                Verified
+              </Badge>
+            </Flex>
+
+            <Heading level={5}>
+              {title}
+            </Heading>
+
+            <Text as="span">
+              {description}
+            </Text>
+          </Flex>
+        </Flex>
+      </Card>
+    </View>)
+}
+
+export default MediaPost;
